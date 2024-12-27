2206d9ca9195b1364b27c6b93d7bf30f539b02f6
feffe1702 <fpm.asd@gmail.com>
2024-12-27 16:53:18 +0100

    [WIP] initialiting work

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap-icons/icons"
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css"
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App

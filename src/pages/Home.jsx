9b00d270c2d9bf948d1de55e2a3f0875c36a2d8f
feffe1702 <fpm.asd@gmail.com>
2024-12-30 09:59:55 +0100

    [WIP] initialating home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 export default function Home() {
     return (
         <>
-            <h2>HOMEPAge</h2>
-
+            <main>
+                <div className="container mt-5">
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
+                        <div className="col">Visualizza il tuo inventario</div>
+                        <div className="col">Aggiorna il tuo inventario</div>
+                        <div className="col">Fast Scan</div>
+                        <div className="col">Imposta quantità minime</div>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }

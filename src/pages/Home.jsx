86e3cafd807666e29c6f954eeadaf07365a1c1b9
feffe1702 <fpm.asd@gmail.com>
2025-01-06 17:34:37 +0100

    [WIP] //

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,9 @@ export default function Home() {
             <div className={style.background}>
                 <div className={style.layer}>
                     <Navbar />
-                    <div className="container d-flex justify-content-center align-items-center w-100 h-100">
-                        <p className={style.title}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia cumque ad enim assumenda fugit est possimus repellat pariatur similique reiciendis?</p>
+                    <div className="container d-flex align-items-center flex-column w-100">
+                        <p className={style.title}>Fast and easy administration for your storage</p>
+                        <button className="btn btn-warning col-6">Get Started!</button>
 
                     </div>
                 </div>
